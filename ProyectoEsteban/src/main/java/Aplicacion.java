9419c1deb3de2c7229060c964426fe139ad864c7
CristinaB93 <cberrocalm93@gmail.com>
2026-02-22 16:55:22 +0100

    He añadido un Syso a cada un a de las clases!

---
+++ ProyectoEsteban/src/main/java/Aplicacion.java
@@ -0,0 +1,7 @@
+public class Aplicacion {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde Desarrollo");
+        
+        System.out.println("Aplicación modificada por Cristina Berrocal :)");
+    }
+}
---
+++ ProyectoEsteban/src/main/java/NuevaFuncionalidad.java
@@ -0,0 +1,7 @@
+public class NuevaFuncionalidad {
+    public void test() {
+        System.out.println("Funcionalidad extra");
+        
+        System.out.println("Funcionalidad modificada por Cristina Berrocal :)");
+    }
+}
